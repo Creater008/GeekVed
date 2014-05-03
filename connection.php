03d99011723641be6d6cbb19e2182ca24cf12ab1
Nishant <nishant_cs@hotmail.com>
2014-05-03 07:34:49 +0530

    Db update
    Database name update.

--- connection.php
+++ connection.php
@@ -2,11 +2,11 @@
 	define('DB_HOST', 'localhost');
     define('DB_USER', '');
     define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'test');
+    define('DB_DATABASE', 'geekve');
 
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	if(!$link) {
-		die('Failed to connect to server: ' . mysql_error());
+		die('Failed to connect to server contact developer for assistance: ' . mysql_error());
 	}
 	
 	//Select database
